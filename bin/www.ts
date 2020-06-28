99b7c2c88366dbd01b57d8ea0a8bba80d14a2b5a
Saif Ali Khan <saffellikhan@github.com>
2020-06-28 21:59:32 +0500

    HTTPS Added and miner fixes

--- bin/www.ts
+++ bin/www.ts
@@ -31,49 +31,56 @@ let normalizePort = (port: number | string) => {
     return false;
 }
 
-// Get port from environment and store in Express.
-const SERVER_PORT: string | number | false = normalizePort(epic.ENV.PORT || '3000');
-epic.APP.set('port', SERVER_PORT);
+// Listen to a particular server
+let listen = (SERVER: http.Server | https.Server, PORT: string | number | false) => {
+    // Listen on provided port, on all network interfaces.
+    SERVER.listen(PORT);
+    // Event listener for HTTP server "error" event.
+    SERVER.on('error', (error: any) => {
+        if (error.syscall !== 'listen') {
+            throw error;
+        }
+        var bind = typeof PORT === 'string'
+            ? 'Pipe ' + PORT
+            : 'Port ' + PORT;
+        // Handle specific listen errors with friendly messages
+        switch (error.code) {
+            case 'EACCES':
+                console.error(bind + ' requires elevated privileges');
+                process.exit(1);
+                break;
+            case 'EADDRINUSE':
+                console.error(bind + ' is already in use');
+                process.exit(1);
+                break;
+            default:
+                throw error;
+        }
+    });
+
+    // Event listener for HTTP/HTTPS server "listening" event.
+    SERVER.on('listening', () => {
+        var addr = SERVER.address();
+        var bind = typeof addr === 'string'
+            ? 'pipe ' + addr
+            : 'port ' + addr?.port;
+        SERVER_DEBUG('Listening on ' + bind);
+        epicEvents.emit("listening", epic);
+        console.log("Server Listening At Port: " + PORT);
+    });
+}
 
 // Create HTTP server.
-export const SERVER = epic.ENV.HTTPS ? https.createServer({
-    cert: fs.readFileSync(path.join(__dirname, epic.ENV.SSL_DIR || 'ssl', epic.ENV.SSL_CERT || 'server.crt')),
-    key: fs.readFileSync(path.join(__dirname, epic.ENV.SSL_DIR || 'ssl', epic.ENV.SSL_KEY || 'server.key')),
-}, epic.APP) : http.createServer(epic.APP);
+if (epic.ENV.HTTP) {
+    let HTTP = http.createServer(epic.APP);
+    listen(HTTP, normalizePort(epic.ENV.HTTP || '80'));
+}
 
-// Listen on provided port, on all network interfaces.
-SERVER.listen(SERVER_PORT);
-
-// Event listener for HTTP server "error" event.
-SERVER.on('error', (error: any) => {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
-    var bind = typeof SERVER_PORT === 'string'
-        ? 'Pipe ' + SERVER_PORT
-        : 'Port ' + SERVER_PORT;
-    // handle specific listen errors with friendly messages
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
-});
-
-// Event listener for HTTP server "listening" event.
-SERVER.on('listening', () => {
-    var addr = SERVER.address();
-    var bind = typeof addr === 'string'
-        ? 'pipe ' + addr
-        : 'port ' + addr?.port;
-    SERVER_DEBUG('Listening on ' + bind);
-    epicEvents.emit("listening", epic);
-    console.log("Server Listening At Port: " + SERVER_PORT);
-});
+// Create HTTPS server.
+if (epic.ENV.HTTPS) {
+    let HTTPS = https.createServer({
+        cert: fs.readFileSync(path.join(__dirname, epic.ENV.SSL_DIR || 'ssl', epic.ENV.SSL_CERT || 'server.crt')),
+        key: fs.readFileSync(path.join(__dirname, epic.ENV.SSL_DIR || 'ssl', epic.ENV.SSL_KEY || 'server.key')),
+    }, epic.APP);
+    listen(HTTPS, normalizePort(epic.ENV.HTTPS || '443'));
+}
